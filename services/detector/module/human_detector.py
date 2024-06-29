ad68ef66ce6dec726b7f55d63a736b5701677ba4
Ha Thien Loc <ha.thienloc131@gmail.com>
2024-06-29 17:36:49 +0700

    add human label

--- services/detector/module/human_detector.py
+++ services/detector/module/human_detector.py
@@ -4,28 +4,47 @@ import cv2
 import base64
 import io
 import numpy as np
+import os
+from services.beer_vlm.language_model import ChatGPT
+from services.beer_vlm.prompter import *
+from services.beer_vlm.logger import LogManager
+from services.beer_vlm.utils import encode_image
 
 POSM_CLASS = [0]
 ID2NAME = {0: 'person'}
 
 class HumanDetector:
-    def __init__(self, model_path = ""):
+    def __init__(self,  model_path = "weights/yolov10b.pt"):
+        self.log_mng = LogManager("test.log", level="debug")
         self.model = YOLOv10(model_path)
 
-    def detect_for_prompter(self, img):
+        self.lm = ChatGPT(log_mng=self.log_mng)
+        self.ps_prompter = PersonPrompter(self.lm)
+        self.prompt_executor = PromptExecutor().add_prompter(self.ps_prompter).build()
+
+
+    async def detect_for_prompter(self, img):
         numpy_img = cv2.imread(img)
-        results = self.model(numpy_img)
+        results = self.model(numpy_img, classes=POSM_CLASS, conf=0.5)
         filtered_boxes_v10 = [{"box": box, "class": ID2NAME[int(box.cls)]} for box in results[0].boxes if box.cls in POSM_CLASS]
         croped_imgs = []
         for box in filtered_boxes_v10:
             xyxy = list(map(int, box['box'].xyxy.view(-1).tolist()))
             croped_imgs.append(numpy_img[xyxy[1]:xyxy[3], xyxy[0]:xyxy[2]])
         croped_base64_imgs = []
-        for img in croped_imgs:
-            img = Image.fromarray(img)
+        for _img in croped_imgs:
+            _img = Image.fromarray(_img)
             buffered = io.BytesIO()
-            img.save(buffered, format="JPEG")
+            _img.save(buffered, format="JPEG")
             img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
             croped_base64_imgs.append(img_str)
-        return croped_base64_imgs
+        img = Image.fromarray(numpy_img)
+        buffered = io.BytesIO()
+        img.save(buffered, format="JPEG")
+        main_img = base64.b64encode(buffered.getvalue()).decode("utf-8")
+        
+        labels = await self.prompt_executor.execute(main_img, {"person_images": croped_base64_imgs})
+        labels = labels["person"]
+        
+        return labels
 
--- test.py
+++ test.py
@@ -2,11 +2,14 @@ import os
 import asyncio
 
 from services.detector.module.posm_detector import PosmDetector
+from services.detector.module.human_detector import HumanDetector
 
 
 
 
-detector = PosmDetector()
+
+# detector = PosmDetector()
+detector = HumanDetector()
 answer = asyncio.run(detector.detect_for_prompter("test_img/1.jpg"))
 
 print(answer)

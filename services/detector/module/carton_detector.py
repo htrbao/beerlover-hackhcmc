06910bf4fcdb043e5cbf394eacb79d392b9156a0
Ha Thien Loc <ha.thienloc131@gmail.com>
2024-06-30 03:39:39 +0700

    test

--- services/detector/module/carton_detector.py
+++ services/detector/module/carton_detector.py
@@ -10,17 +10,17 @@ from services.beer_vlm.language_model import ChatGPT
 from services.beer_vlm.prompter import *
 from services.beer_vlm.logger import LogManager
 
-POSM_CLASS = [1]
-ID2NAME = {1: 'beer_keg'}
+POSM_CLASS = [0]
+ID2NAME = {0: 'carton'}
 
 class CartonDetector:
-    def __init__(self, model_path = "weights/posm.pt"):
+    def __init__(self, model_path = "weights/carton.pt"):
         self.log_mng = LogManager("test.log", level="debug")
         self.model = YOLOv10(model_path)
         # self.billboard_model = YOLOv10(billboard_model_path)
 
     def detect(self, numpy_img):
-        results = self.model(numpy_img)
+        results = self.model(numpy_img, conf=0.3)
         # billboard_results = self.billboard_model(numpy_img, save=True)
         filtered_boxes_v10 = [{"box": box, "class": ID2NAME[int(box.cls)]} for box in results[0].boxes if box.cls in POSM_CLASS]
         # billboard_results = [{"box": box, "class": "billboard"} for box in billboard_results[0].boxes]

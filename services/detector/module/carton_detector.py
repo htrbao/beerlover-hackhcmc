5b2ef07334400c7ff2a2e30332cd19781875620c
Ha Thien Loc <ha.thienloc131@gmail.com>
2024-06-30 03:32:08 +0700

    sai class

--- services/detector/module/carton_detector.py
+++ services/detector/module/carton_detector.py
@@ -10,8 +10,8 @@ from services.beer_vlm.language_model import ChatGPT
 from services.beer_vlm.prompter import *
 from services.beer_vlm.logger import LogManager
 
-POSM_CLASS = [2]
-ID2NAME = {2: 'beer_keg'}
+POSM_CLASS = [1]
+ID2NAME = {1: 'beer_keg'}
 
 class CartonDetector:
     def __init__(self, model_path = "weights/posm.pt"):

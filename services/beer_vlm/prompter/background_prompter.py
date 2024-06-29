90539b98bf48fbee907a20cf794ec30f82eecb8e
Ha Thien Loc <ha.thienloc131@gmail.com>
2024-06-30 01:03:03 +0700

    error

--- services/beer_vlm/prompter/background_prompter.py
+++ services/beer_vlm/prompter/background_prompter.py
@@ -64,8 +64,8 @@ Answer the location, atmosphere and emotion of image in JSON format.
         answer["prompt"] = await self.background_prompt_prepare(**answer)
         return answer
     async def query(self, image: Optional[str], **results) -> str:
-        # return self.handle_background(image, **results)
-        try:
-            return await self.handle_background(image, **results)
-        except:
-            return await self.default_background()
+        return self.handle_background(image, **results)
+        # try:
+        #     return await self.handle_background(image, **results)
+        # except:
+        #     return await self.default_background()

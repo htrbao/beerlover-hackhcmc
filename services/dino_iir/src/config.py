2eb3c390caa17419fd82d9a2a22524782307317a
Ha Thien Loc <ha.thienloc131@gmail.com>
2024-06-29 11:40:35 +0700

    dino retriever

---
+++ services/dino_iir/src/config.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+from pydantic_settings import BaseSettings
+
+FILE = Path(__file__)
+ROOT = FILE.parent.parent
+
+
+class Settings(BaseSettings):
+    # API SETTINGS
+    HOST: str
+    PORT: int
+    CORS_ORIGINS: list
+    CORS_HEADERS: list
+
+    # MODEL SETTINGS
+    MODEL_NAME: str = "ViT-B/32"
+    DEVICE: str = "cpu"
+
+    # FAISS DATABASE SETTINGS
+    INDEX_FILE_PATH: str
+    INDEX_SUBFRAMES_FILE_PATH: str
+    KEYFRAMES_GROUPS_JSON_PATH: str
+    SUBFRAMES_GROUPS_JSON_PATH: str
+
+    class Config:
+        env_file = ROOT / ".env"
+
+
+settings = Settings()
---
+++ services/dino_iir/src/iir/vision_model.py
@@ -0,0 +1,27 @@
+from functools import lru_cache
+from typing import Union
+
+import torch
+from transformers import AutoImageProcessor, AutoModel
+from PIL import Image
+
+from services.common import VisionModel
+
+
+class DinoVisionModel(VisionModel):
+    def __init__(self, device: str = "cuda"):
+        name_model = "facebook/dinov2-base"
+        self.processor = AutoImageProcessor.from_pretrained(name_model)
+        self.model = AutoModel.from_pretrained(name_model)
+        self.device = device
+
+    def get_embedding(self, input: Image.Image):
+        if isinstance(input, Image.Image):
+            inputs = self.processor(images=input, return_tensors='pt')
+            vector = self.model(**inputs).pooler_output
+            vector /= vector.norm(dim=-1, keepdim=True)
+            vector = vector.cpu().detach().numpy().astype("float32")
+            return vector
+        else:
+            raise Exception("Invalid input type")
+

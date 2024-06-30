db611de8b319137eef8a977e5f303ee0e25f7e57
htrbao <trbaohoo@gmail.com>
2024-06-30 08:06:24 +0700

    upgrade UI

--- services/client/src/components/Results.js
+++ services/client/src/components/Results.js
@@ -11,7 +11,7 @@ const Results = ({
     beer_posm_infos,
     background,
 }) => {
-    console.log(beer_can_infos)
+    console.log(beer_can_infos);
     return (
         <div className="results-container">
             <div className="analyzed-result-container">
@@ -19,17 +19,18 @@ const Results = ({
                 <div
                     className="analyzed-result-container"
                     style={{ paddingLeft: "20px" }}>
-                    <h2>🍾 Can Statistics</h2>
+                    <h2>🍾 Bottle Statistics</h2>
                     <div className="analyzed-results-list">
-                        {beer_can_infos && beer_can_infos.map((item, index) => (
-                            <DetectInfo
-                                key={index}
-                                beer_line={""}
-                                brand={item.brand}
-                                object_type={item.object_type}
-                                number={item.number}
-                            />
-                        ))}
+                        {beer_can_infos &&
+                            beer_can_infos.map((item, index) => (
+                                <DetectInfo
+                                    key={index}
+                                    beer_line={""}
+                                    brand={item.brand}
+                                    object_type={item.object_type}
+                                    number={item.number}
+                                />
+                            ))}
                     </div>
                 </div>
                 <div
@@ -37,15 +38,16 @@ const Results = ({
                     style={{ paddingLeft: "20px" }}>
                     <h2>📦 Carton Statistics</h2>
                     <div className="analyzed-results-list">
-                        {beer_carton_infos && beer_carton_infos.map((item, index) => (
-                            <DetectInfo
-                                key={index}
-                                beer_line={""}
-                                brand={item.brand}
-                                object_type={item.object_type}
-                                number={item.number}
-                            />
-                        ))}
+                        {beer_carton_infos &&
+                            beer_carton_infos.map((item, index) => (
+                                <DetectInfo
+                                    key={index}
+                                    beer_line={""}
+                                    brand={item.brand}
+                                    object_type={item.object_type}
+                                    number={item.number}
+                                />
+                            ))}
                     </div>
                 </div>
                 <div
@@ -53,15 +55,16 @@ const Results = ({
                     style={{ paddingLeft: "20px" }}>
                     <h2>👫 People Statistics</h2>
                     <div className="analyzed-results-list">
-                        {beer_person_infos && beer_person_infos.map((item, index) => (
-                            <DetectInfo
-                                key={index}
-                                beer_line={item.beer_line}
-                                brand={item.brand}
-                                object_type={item.object_type}
-                                number={item.number}
-                            />
-                        ))}
+                        {beer_person_infos &&
+                            beer_person_infos.map((item, index) => (
+                                <DetectInfo
+                                    key={index}
+                                    beer_line={item.beer_line}
+                                    brand={item.brand}
+                                    object_type={item.object_type}
+                                    number={item.number}
+                                />
+                            ))}
                     </div>
                 </div>
                 <div
@@ -69,27 +72,99 @@ const Results = ({
                     style={{ paddingLeft: "20px" }}>
                     <h2>♺ POSM Statistics</h2>
                     <div className="analyzed-results-list">
-                        {beer_posm_infos && beer_posm_infos.map((item, index) => (
-                            <DetectInfo
-                                key={index}
-                                beer_line={""}
-                                brand={item.brand}
-                                object_type={item.object_type}
-                                number={item.number}
-                            />
-                        ))}
+                        {beer_posm_infos &&
+                            beer_posm_infos.map((item, index) => (
+                                <DetectInfo
+                                    key={index}
+                                    beer_line={item.beer_line}
+                                    brand={item.brand}
+                                    object_type={item.object_type}
+                                    number={item.number}
+                                />
+                            ))}
                     </div>
                 </div>
             </div>
             <div className="analyzed-result-container" style={{ height: 400 }}>
                 <h1>Distributions</h1>
                 <div className="distribution-list" style={{ height: 400 }}>
-                    {beer_can_infos !== undefined && beer_can_infos.length > 1 && <PieChart beer_infos={beer_can_infos} />}
-                    {beer_carton_infos !== undefined && beer_carton_infos.length > 1 && <PieChart beer_infos={beer_carton_infos} />}
+                    {beer_can_infos !== undefined &&
+                        beer_can_infos.length > 1 && (
+                            <PieChart beer_infos={beer_can_infos} />
+                        )}
+                    {beer_carton_infos !== undefined &&
+                        beer_carton_infos.length > 1 && (
+                            <PieChart beer_infos={beer_carton_infos} />
+                        )}
+                </div>
+            </div>
+            <div
+                className="analyzed-result-container"
+                style={{ flexDirection: "row" }}>
+                <h1>Background</h1>
+                <div className="analyzed-results-list">
+                    <div
+                        className="background-results-container"
+                        style={{ marginLeft: "4%", marginTop: "10px" }}>
+                        <h4
+                            style={{
+                                width: "fit-content",
+                                backgroundColor: "#f8f8f8",
+                                borderRadius: "10px",
+                                marginTop: "-10px",
+                                paddingLeft: "5px",
+                                paddingRight: "5px",
+                            }}>
+                            Activity and Location
+                        </h4>
+                        <p style={{ alignItems: "center", paddingLeft: "20%" }}>
+                            {background.activity
+                                ? background.activity + " at"
+                                : ""}{" "}
+                            {background.location}
+                        </p>
+                    </div>
+                    <div
+                        className="background-results-container"
+                        style={{ marginLeft: "4%", marginTop: "10px" }}>
+                        <h4
+                            style={{
+                                width: "fit-content",
+                                backgroundColor: "#f8f8f8",
+                                borderRadius: "10px",
+                                marginTop: "-10px",
+                                paddingLeft: "5px",
+                                paddingRight: "5px",
+                            }}>
+                            Atmosphere
+                        </h4>
+                        <p style={{ alignItems: "center", paddingLeft: "20%" }}>
+                            {background.atmosphere.length > 0 && background.atmosphere[0]} {background.atmosphere.length > 1 && <span>&#183;</span>} {background.atmosphere.length > 1 && background.atmosphere[1]} {background.atmosphere.length > 2 && <span>&#183;</span>} {background.atmosphere.length > 2 && background.atmosphere[2]}
+                        </p>
+                    </div>
+                    <div
+                        className="background-results-container"
+                        style={{ marginLeft: "4%", marginTop: "10px" }}>
+                        <h4
+                            style={{
+                                width: "fit-content",
+                                backgroundColor: "#f8f8f8",
+                                borderRadius: "10px",
+                                marginTop: "-10px",
+                                paddingLeft: "5px",
+                                paddingRight: "5px",
+                            }}>
+                            Emotions
+                        </h4>
+                        <p style={{ alignItems: "center", paddingLeft: "20%" }}>
+                            {background.emotion.length > 0 && background.emotion[0]} {background.emotion.length > 1 && <span>&#183;</span>} {background.emotion.length > 1 && background.emotion[1]} {background.emotion.length > 2 && <span>&#183;</span>} {background.emotion.length > 2 && background.emotion[2]}
+                        </p>
+                    </div>
                 </div>
             </div>
             <div className="analyzed-result-container">
-                <h1>Background</h1>
+                <h1>Processed Image</h1>
+
             </div>
         </div>
     );
